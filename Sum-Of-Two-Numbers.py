38a1375650dbed8d0063e1d55d8ecc06db7fc818
PhyscoLite <145963139+PhyscoLite@users.noreply.github.com>
2023-09-30 14:23:21 +0530

    Sum-Of-Two-Numbers.py

--- Sum-Of-Two-Numbers.py
+++ Sum-Of-Two-Numbers.py
@@ -2,3 +2,4 @@
 A = int (input ("Enter your Fisrt No: "))      # We have printed 'Enter your first number:' and also used the input function, saving its result in a variable named 'A'.
 B = int (input ("Enter your Second No: "))
 print ("Total sum is: " , A + B)
+# Similarly, we can use '-' for subtraction, '*' for multiplication, and '/' for division.
