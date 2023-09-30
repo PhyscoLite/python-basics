545bc4c5c94422bae57ebf1f9bfea437519cea70
PhyscoLite <145963139+PhyscoLite@users.noreply.github.com>
2023-09-30 13:39:38 +0530

    Taking-Input-From-User.py

---
+++ Taking-Input-From-User.py
@@ -0,0 +1,3 @@
+# Prompt for taking input from user.
+A = input("What is your name: ")      # "A" is a veriable to store the input from user and we also use Print Function (" ") for making good user experience.
+print ("Hello," , A)      # now we the "Hello with our earlier veriable A.
