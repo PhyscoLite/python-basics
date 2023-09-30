07f0c59ab1b9899bdfc3d9bd487ae66cb6516dac
PhyscoLite <145963139+PhyscoLite@users.noreply.github.com>
2023-09-30 14:17:31 +0530

    Sum-Of-Two-Numbers.py

---
+++ Sum-Of-Two-Numbers.py
@@ -0,0 +1,4 @@
+# using 'int' for numbers.
+A = int (input ("Enter your Fisrt No: "))      # We have printed 'Enter your first number:' and also used the input function, saving its result in a variable named 'A'.
+B = int (input ("Enter your Second No: "))
+print ("Total sum is: " , A + B)
