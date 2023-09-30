05a0cf97ec9a11ab4a6c651bd9a1d2b982b7ca77
PhyscoLite <145963139+PhyscoLite@users.noreply.github.com>
2023-09-30 14:38:09 +0530

    Area-Of-Triangle.py

---
+++ Area-Of-Triangle.py
@@ -0,0 +1,5 @@
+# Input the base and height of the triangle from the user. Formula for Area of Triangle is '(base x height)/2' or '(base x height) x 0.5
+Base = float(input("Enter the length of the base of the triangle: "))
+Height = float(input("Enter the height of the triangle: "))
+Area = 0.5 * Base * Height       # Here, there are Three variables, 'base' and 'height' and 'Area'.
+print ("Area of triangle is: ", Area)
