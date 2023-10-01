a0da1d992879a25a643e56d8b72deabb8e2cc3b6
Nishant <72150854+Future423@users.noreply.github.com>
2023-10-01 06:30:18 +0530

    Update Taking-Input-From-User.py

--- Taking-Input-From-User.py
+++ Taking-Input-From-User.py
@@ -1,3 +1,8 @@
-# Prompt for taking input from user.
-A = input("What is your name: ")      # "A" is a veriable to store the input from user and we also use Print Function (" ") for making good user experience.
-print ("Hello," , A)      # now we the "Hello with our earlier veriable A.
+# Syntax for using input function
+A = input("What is your name: ") # Here veriable "A" stores the value inputted by the user
+print ("Hello, " , A) # Here we are concating the name which is stored in "A" with "Hello"
+
+input("Enter any key to continue") # Here we are taking input but not saving it in a veriable
+
+#input()
+#it will print nothing but take input
