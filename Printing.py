bdc06c2a8a144f9cca67571b3126572b367b1774
Shant <113237039+Shant2324@users.noreply.github.com>
2023-10-01 08:13:10 +0530

    Create Printing.py

---
+++ Printing.py
@@ -0,0 +1,20 @@
+#diffrent use of print function
+
+#printing messages
+print("Hey there!")
+#or
+print('Hey again')
+
+#printing blank line
+print()
+
+#printing value from eriable 
+A="Hello"
+print(A)
+
+#printing multiline
+print('''this 
+is 
+for 
+printing 
+multiline''')
