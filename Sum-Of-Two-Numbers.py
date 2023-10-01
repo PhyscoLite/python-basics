e924dc75dcfe8e99067ae7f79d2efa558b7828a6
Spider <72209773+Cyberhero747@users.noreply.github.com>
2023-10-01 07:20:04 +0530

    Update Sum-Of-Two-Numbers.py

--- Sum-Of-Two-Numbers.py
+++ Sum-Of-Two-Numbers.py
@@ -1,5 +1,13 @@
-# using 'int' for numbers.
-A = int (input ("Enter your Fisrt No: "))      # We have printed 'Enter your first number:' and also used the input function, saving its result in a variable named 'A'.
+# using 'int' function for numeric values.
+
+A = int (input ("Enter your Fisrt No: "))      
+# We have printed 'Enter your first number:' and saving the input in a variable named 'A'.
+#Simillary for 'B'
 B = int (input ("Enter your Second No: "))
-print ("Total sum is: " , A + B)
-# Similarly, we can use '-' for subtraction, '*' for multiplication, and '/' for division.
+print ("The Sum of given numbers is: " , A + B)
+
+#Using third veriable
+#C=A+B
+#print("The Sum of given numbers is: ",C)
+
+# Similarly, we can use '-' for subtraction, '*' for multiplication and so on...
